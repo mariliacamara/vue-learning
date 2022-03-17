b66b108b96776a89a2e0461f5febe64d8f740341
Marilia <mariliacamara@ibm.com>
2022-03-16 22:18:15 -0300

    Learning about vue lifecycle

--- ZeroToMastery/section-04/section-4-1-starter/app.js
+++ ZeroToMastery/section-04/section-4-1-starter/app.js
@@ -1,15 +1,45 @@
 let vm = Vue.createApp({
   data() {
     return {
-      message: "Hello people!"
-    }
-  }
-})
-
+      message: "Hello, people! 🚀",
+    };
+  },
+  beforeCreate() {
+    // this is undefined because the beforeCreate function doesn't have access to any data yet!
+    console.log("👾 beforeCreate() called!", this.message);
+  },
+  created() {
+    // created returns this.message
+    console.log("👾 created() called!", this.message);
+  },
+  beforeMount() {
+    // returns null as hook is triggered after created but before the app is mounted in the front end.
+    console.log("🚀 beforeMount() called", this.$el);
+  },
+  mounted() {
+    // returns the mounted el
+    console.log("🚀 mounted() called", this.$el);
+  },
+  beforeUpdate() {
+    // likely used for debugging purposes
+    console.log("👁 beforeUpdate() called");
+  },
+  updated() {
+    // likely used for debugging purposes
+    console.log("👁 updated() function called!");
+  },
+  beforeUnmount() {
+    console.log("☠️ beforeUnmount() function called!");
+  },
+  unmounted() {
+    console.log("☠️ unmounted() function called!");
+  },
+  // !IMPORTANT: Search for more informations about beforeUnmount and unmounted lifecycles.
+});
 
 vm.mount('#app');
 
-// !IMPORTANT Mounting the element after 3s
+// ?Noted: Mounting the element after 3s
 // setTimeout(() => {
 //   vm.mount('#app');
 // }, 3000);
