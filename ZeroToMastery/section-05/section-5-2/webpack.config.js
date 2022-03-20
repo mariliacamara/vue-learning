4212b32c87bf005a79e56032003a9362b338194e
mariliacamara <mariliacamara.dev@gmail.com>
2022-03-20 17:48:45 -0300

    Install sass and config babel

--- ZeroToMastery/section-05/section-5-2/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // entry file is considered the main file for the application
-  // the entry files responsibility is to load other files we'll need for our application to function
-  entry: './index.js',
-  output: {
-    filename: 'bundle.js', // creates a file that contains all the js code
-    path: __dirname + '/dist', // save the bundle.js into the respective directory
-  }
-}
---
+++ ZeroToMastery/section-05/webpack.config.js
@@ -0,0 +1,29 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js', 
+    path: __dirname + '/dist',
+  },
+  plugins: [new MiniCssExtractPlugin({
+    filename: "main.css"
+  })],
+  module: {
+    rules: [
+      {
+       test: /\.js%/,
+       exclude: /(node_modules)/,
+       use: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, 
+          "css-loader", 
+          "sass-loader"
+        ]
+      }
+    ],
+  }
+}
